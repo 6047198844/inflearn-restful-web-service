44e61ff12f22b976c425cd5ed2f7d55ffa43f16a
6047198844 <jslee@miridih.com>
2022-02-16 22:56:17 +0900

    feat: Initial commit 🎉

---
+++ src/main/java/com/example/inflearnrestfulwebservice/InflearnRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnRestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnRestfulWebServiceApplication.class, args);
+    }
+
+}
