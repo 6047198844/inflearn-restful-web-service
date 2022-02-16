8c760f6a966383380173f5181476985f26f7a8e7
6047198844 <jslee@miridih.com>
2022-02-16 22:53:18 +0900

    feat: Initial commit 🎉

---
+++ src/test/java/com/example/inflearnrestfulwebservice/InflearnRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
