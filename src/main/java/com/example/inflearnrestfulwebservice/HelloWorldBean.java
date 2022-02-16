8473a4bf30d8201b4f379c49933d3549204ef4f5
6047198844 <jslee@miridih.com>
2022-02-16 22:55:58 +0900

    feat: Add HelloWorld✨

---
+++ src/main/java/com/example/inflearnrestfulwebservice/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestfulwebservice;
+// lombok
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // getter/setter 자동 생성
+@AllArgsConstructor // 필드값이 모두 있는 생성자
+@NoArgsConstructor // 디폴드 생성자
+public class HelloWorldBean {
+    private String message;
+}
---
+++ src/main/java/com/example/inflearnrestfulwebservice/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.example.inflearnrestfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    // == @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    // RestController -> JSON 형태로 자바빈 반환
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    // hello-world-bean/path-variable/kenneth
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
