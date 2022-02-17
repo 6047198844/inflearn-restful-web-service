847fe804e5d1b10016b6a709f841913cfeb1b3df
6047198844 <jslee@miridih.com>
2022-02-18 01:27:44 +0900

    feat: Add delete function ✨

--- src/main/java/com/example/inflearnrestfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/inflearnrestfulwebservice/user/UserDaoService.java
@@ -1,9 +1,14 @@
 package com.example.inflearnrestfulwebservice.user;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
+// 스테레오 타입. Component 보다 명확하게 Service 를 이용한다.
+@Service
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
 
@@ -31,6 +36,21 @@ public class UserDaoService {
     public User findOne(int id) {
         for (User user : users) {
             if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public User deleteById(int id) {
+        final Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+
+            if (user.getId() == id) {
+                iterator.remove();
                 return user;
             }
         }
