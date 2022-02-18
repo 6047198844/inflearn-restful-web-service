9e10d78d52c4a5fb646ead893d19edca5b21ffc5
6047198844 <jslee@miridih.com>
2022-02-19 00:27:01 +0900

    feat: Add delete API ✨

--- src/main/java/com/example/inflearnrestfulwebservice/user/UserController.java
+++ src/main/java/com/example/inflearnrestfulwebservice/user/UserController.java
@@ -1,12 +1,15 @@
 package com.example.inflearnrestfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class UserController {
-    private UserDaoService service;
+    private final UserDaoService service;
 
     public UserController(UserDaoService service) {
         this.service = service;
@@ -20,11 +23,34 @@ public class UserController {
     // GET /users/1 or /users/10 -> String -> Convert -> int
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        final User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+
+        // 응답코드값 제어
+        final URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}") // 가변 변수
+                .buildAndExpand(savedUser.getId()) // 대입
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
     }
 }
