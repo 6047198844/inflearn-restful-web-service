5149430aa8763f82a5e777355fdd7c4ad89bfe8d
6047198844 <jslee@miridih.com>
2022-02-23 00:36:39 +0900

    feat: Add jpa ✨

--- src/main/java/com/example/inflearnrestfulwebservice/user/User.java
+++ src/main/java/com/example/inflearnrestfulwebservice/user/User.java
@@ -9,17 +9,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+//@JsonFilter("UserInfo")
+//@JsonIgnoreProperties(value = {"password"})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
-//@JsonFilter("UserInfo")
-//@JsonIgnoreProperties(value = {"password"})
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name 은 2글자 이상 입력해주세요.")
